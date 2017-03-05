c0ddf2e51a5b5f251cc8698e582f1be722473b78
LeeMinglu <402007353@qq.com>
2017-03-05 16:04:55 +0800

    添加代码

---
+++ PhoneStudy/PhoneStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhoneStudy
+//
+//  Created by 李明禄 on 2017/3/5.
+//  Copyright © 2017年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhoneStudy/PhoneStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhoneStudy
+//
+//  Created by 李明禄 on 2017/3/5.
+//  Copyright © 2017年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
